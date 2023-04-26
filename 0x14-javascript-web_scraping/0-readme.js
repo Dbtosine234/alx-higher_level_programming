56d31078c41e066dc1ce0ae5e74e15ff47ff71b1
Dbtosine234 <dbtosine4real@gmail.com>
2023-04-26 23:29:24 +0000

    web_scrapping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,4 +1,6 @@
-#!/bin/bash  
-  
-value=`cat cisfun`  
-echo "$value" 
+#!/usr/bin/node
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) console.log(err);
+  else console.log(data);
+});
